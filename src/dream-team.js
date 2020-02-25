f7c8ae9b054f051219754b6d6ab0418541d9c3b2
Yauheni Kozak <zhenikusss@gmail.com>
2020-02-25 20:14:06 +0300

    feat: implement function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+  if (Array.isArray(arr) === false) { 
+    return false;
+  } else {
+    let newArray = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'string') {
+          arr[i] = arr[i].toUpperCase();
+          arr[i] = arr[i].trim();
+          arr[i] = arr[i].substring(0, 1);
+          newArray.push(arr[i]);  
+        }
+      } 
+    return newArray.sort().join('');
+  }
+}
